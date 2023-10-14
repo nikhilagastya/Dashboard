5c60a55053ae5657f62aecb4e44647df48193a91
nikhilagastya <karthikagastya2002@gmail.com>
2023-10-15 01:16:05 +0530

    added bulk update

--- src/Components/Change_Tid.js
+++ src/Components/Change_Tid.js
@@ -39,10 +39,27 @@ export default function Change_Tid() {
       });
   };
 
+  const bulk_update = () => {
+    // Send a request to update the tid on the backend
+    let lis=["20BD1A0501","20BD1A050J"]
+    for(let i=0;i<lis.length;i++){
+    axios.post('https://pass-server-o7gu.onrender.com/put_council_perf', { rno:lis[i], t_id: "Council/Volunteers" })
+      .then((response) => {
+        console.log(lis[i]+' Tid updated successfully');
+        // Optionally, you can update the personData with the new tid here
+      })
+      .catch((error) => {
+        console.error('Error updating tid:', error);
+      });
+    }
+  };
+
   return (
     <div>
+      <button onClick={bulk_update}>Bulk update</button>
       <h1>Change Tid</h1>
       <div>
+
         <label>Enter Roll Number: </label>
         <input type="text" value={roll} onChange={handleRollChange} />
         <button onClick={fetchPersonData}>Fetch Data</button>
